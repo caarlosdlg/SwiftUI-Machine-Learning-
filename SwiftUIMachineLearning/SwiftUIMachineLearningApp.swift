847df3c72100be94cb44eb87ec849146310a5eba
caarlosdlg <caarllossdlg@gmail.com>
2024-12-16 23:26:36 -0600

    Initial Commit

---
+++ SwiftUIMachineLearning/SwiftUIMachineLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMachineLearningApp.swift
+//  SwiftUIMachineLearning
+//
+//  Created by Carlos Delgado on 16/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMachineLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
